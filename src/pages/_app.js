2de1e90dd5f53f7578b683596d86dd8f077a4ecb
shravan <NALLASHRAVAN.REDDY@kohler.com>
2025-11-12 19:06:41 +0530

    sravan here intraduce navigation and routes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+<div className="min-h-screen flex flex-col">
+   <Navbar />
+      <main className="flex-1 container max-w-7xl mx-auto p-8">
+        <Component {...pageProps} />
+      </main>
+    <Footer />
+  </div>
+
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -43,6 +43,7 @@ export default function Home() {
           priority
         />
       </div>
+      <h1>local running</h1>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
